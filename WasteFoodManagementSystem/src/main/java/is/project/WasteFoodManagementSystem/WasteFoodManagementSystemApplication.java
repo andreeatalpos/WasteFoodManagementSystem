fccd6856e3fdda8b93edc7eeaa745cb0d6f058e8
Andreea Talpos <andreea.talpos@student.utcluj.ro>
2021-12-07 20:30:30 +0200

    First commit

---
+++ WasteFoodManagementSystem/src/main/java/is/project/WasteFoodManagementSystem/WasteFoodManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package is.project.WasteFoodManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasteFoodManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WasteFoodManagementSystemApplication.class, args);
+	}
+
+}
---
+++ WasteFoodManagementSystem/src/test/java/is/project/WasteFoodManagementSystem/WasteFoodManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package is.project.WasteFoodManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasteFoodManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
